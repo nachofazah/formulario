f7302b55af0dad2991810233941484df2afa1576
nachofazah <ignaciomartin.fazahbeiroa@estudiantes.unahur.edu.ar>
2023-06-29 01:44:50 -0300

    formulario

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import React, {Fragment, useEffect, useState} from 'react';
+import Formulario from './components/Formulario'
+import Cliente from './components/Cliente'
 
 function App() {
+
+  //generar hook vacio
+  const [clientes, editarClientes] = useState([]);
+
+  //hook useEffect: sirve para ejecutar alguna funcionalidad
+
+  useEffect( () => {
+
+  }, []);
+
+  //funcion que toma el socio nuevo
+  const agregarCliente = (socio) => {
+    editarClientes([
+          ...clientes,
+          socio
+    ])
+  };
+
+  //borrar cliente
+
+  const borrarCliente = (id) => {
+    const nuevosClientes = clientes.filter(cliente => cliente.id !== id)
+    editarClientes(nuevosClientes);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Container>
+        <Row>
+          <Col><h2>Preinscripción al curso</h2></Col>
+        </Row>
+        <Row>
+          <Col>
+            <Formulario
+              agregarCliente={agregarCliente}
+            />
+          </Col>
+          <Col>
+            {
+              clientes.length > 0 ?
+              <h3>listado de estudiantes</h3>
+              :
+              <h3>no hay estudiantes inscriptos</h3>
+            }
+            {
+              clientes.map( cliente =>
+                <Cliente
+                  cliente={cliente}
+                  key={cliente.id}
+                  borrarCliente={borrarCliente}
+                />
+                )
+            }
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
   );
 }
 
 export default App;
+
